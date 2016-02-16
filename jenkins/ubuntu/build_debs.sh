8cf857649e89b201177095d7a3cb6e0e0f602e5a
Bert De Jonghe <Bert.DeJonghe@openvstorage.com>
2016-02-16 12:30:57 +0100

    jenkins build support (ubuntu 14.04)

---
+++ jenkins/ubuntu/build_debs.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+SRCDIR="$1"
+
+if [ $# -ne 1 ]
+then
+  echo "Usage: $0 <debianized_source_dir>"
+  exit 1
+fi
+
+if [ ! -d "${SRCDIR}" ] 
+then
+  echo "Sourcedir ${SRCDIR} not found?! Aborting build..."
+  exit 1
+fi
+
+set -e
+
+echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
+
+## we know we depend on the volumedriver-dev package, and we want latest greatest, so install from artifact
+
+for p in volumedriver-base_*_amd64.deb volumedriver-server_*_amd64.deb volumedriver-dev_*_amd64.deb
+do
+  echo -n "Installing $p..."
+  sudo dpkg -i $p &>/dev/null || sudo apt-get install -qq -y -f >/dev/null
+  [ $? -ne 0 ] && echo 'FAILED' || echo 'OK'
+done
+
+cd ${SRCDIR}
+
+echo ">>> INSTALLING BUILD DEPENDENCIES <<<"
+sudo apt-get install -y $(dpkg-checkbuilddeps 2>&1 | sed -e 's/.*dependencies://' -e 's/ ([^)]*)/ /g') 
+
+echo ">>> BUILDING DEBIAN PACKAGES <<<"
+dpkg-buildpackage -us -uc -b
+
+## .deb && *.ddeb packages should be available now
